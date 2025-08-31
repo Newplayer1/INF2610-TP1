17cf346d2552b08ef6ea8e0f138aa289da25ef3a
Antoine <antoinebourassa@gmail.com>
2025-08-31 15:32:12 -0400

    first commit

---
+++ section3/section3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+
+
+int main() {
+
+    return 0;
+}
